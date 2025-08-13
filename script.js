90e3009afe2546f201d6528ead2b93bd06087803
Kamal-Kant11 <kamalkantsinghal333@gmail.com>
2025-08-13 06:41:54 +0530

    Fix the display of digital clock.

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ setInterval(()=>{
     document.querySelector(".mhand").style.transform = `rotate(${minsRot}deg)`;
     document.querySelector(".shand").style.transform = `rotate(${secsRot}deg)`;
 
-    h.innerHTML = hrs;
-    m.innerHTML = mins;
-    s.innerHTML = secs;
+    h.innerHTML = `hrs <10 ? 0${hrs} : hrs`;
+    m.innerHTML = `mins <10 ? 0${mins} : mins`;
+    s.innerHTML = `secs <10 ? 0${secs} : secs`;
 },1000)
+
